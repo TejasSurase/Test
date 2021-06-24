7d5ef54730750c8ab7cb1006158c0750ebce7f60
shlok <surasetejas63@gmail.com>
2021-06-24 19:06:48 +0530

    practice

---
+++ one.js
@@ -0,0 +1,8 @@
+function getEvenNumber() {
+    var arr = [4,5,7,8,14,45,76];
+
+    var evens = arr.filter(number = number % 2 ==0);
+    document.write('Even numbers ;' + evens);
+
+}
+getEvenNumber();
